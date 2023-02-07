1ee3265f931a9ea894e06deabd494c22e44edc71
Andrea Baraldi <baraldian@gmail.com>
2023-02-07 15:41:35 +0100

    add: experiment_no_run_lin_prog.py

---
+++ experiment_no_run_lin_prog.py
@@ -0,0 +1,26 @@
+import sys
+from run import execute_experiment
+from utils_values import fractions, sample_variation, eps, dataset_names
+
+
+if __name__ == "__main__":
+    original_argv = sys.argv.copy()
+
+    for dataset_name in dataset_names:
+        args = [dataset_name, 'model_name',
+                '--exp_subset','--redo_exp',
+                '--no_run_linprog_step',
+                ]
+        kwargs = {}
+
+        args[1] = 'hybrids'
+        kwargs.update(**{
+            '--eps': eps,
+            '--sample_variations': sample_variation,
+            '--exp_fractions': fractions,
+            '--exp_grid_ratio': 'sqrt',
+
+            # '--grid_fractions': fixed_sample_frac
+            # '--base_model_code': 'gbm',  # 'lgmb
+        })
+        execute_experiment(args, kwargs, original_argv)
