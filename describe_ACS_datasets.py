5bf5b4e4d764fe010b118c1a9efe558dd13b6d98
Andrea Baraldi <baraldian@gmail.com>
2023-02-28 17:00:09 +0100

    add: memory usage info to describe datasets.

--- describe_ACS_datasets.py
+++ describe_ACS_datasets.py
@@ -19,11 +19,13 @@ if __name__ == '__main__':
                         'ACSIncome',
                         ]:
         loader_method = getattr(folktables, dataset_str)
-        X, y, A = load_transform_ACS(loader_method=loader_method, return_acs_data=True)
+        X, y, A, acs_data = load_transform_ACS(loader_method=loader_method, return_acs_data=True)
         # acs_data.iloc[:1000].to_csv(f'results/fairlearn-2/ACSDataset_descriptions/ACSPublicCoverage_h1000.csv')
         # acs_data.iloc[:1000].loc[:, ['HISP', 'RAC1P', 'PUBCOV']].to_csv(f'results/fairlearn-2/ACSDataset_descriptions/ACSPublicCoverage_h1000_race.csv')
+        mem_usage = X.memory_usage().sum() / (2 ** (10 * 3))
         t_dict.update(dataset_name=dataset_str, size=X.shape[0], columns=X.shape[1], sensitive_attr=A.name,
-                      sensitive_attr_nunique=A.nunique(), target_col=y.name, sensitive_attr_unique_values=A.unique())
+                      sensitive_attr_nunique=A.nunique(), target_col=y.name, sensitive_attr_unique_values=A.unique(),
+                      mem_usage=mem_usage)
         desc = X.describe().join([y.describe(), A.describe()])
         desc.to_csv(os.path.join(descriptions_dir, dataset_str + 'describe.csv'))
         dict_list.append(t_dict.copy())
