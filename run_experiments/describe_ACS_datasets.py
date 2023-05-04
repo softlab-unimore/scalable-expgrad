465b0819134c8a97a15e348f1a5de1c1e65f7e38
Andrea Baraldi <baraldian@gmail.com>
2023-05-04 14:33:57 +0200

    add: load and save best parameters for finetuning of lr for each dataset

--- run_experiments/describe_ACS_datasets.py
+++ run_experiments/describe_ACS_datasets.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 
 import folktables
+import utils_experiment
 from utils_prepare_data import load_transform_ACS
 from run import ExperimentRun
 
@@ -12,11 +13,7 @@ if __name__ == '__main__':
     os.makedirs(descriptions_dir, exist_ok=True)
     dict_list = []
     t_dict = {}
-    for dataset_str in [ 'ACSHealthInsurance', 'ACSEmployment',
-                'ACSEmploymentFiltered', 'ACSIncomePovertyRatio',
-                        'ACSMobility', 'ACSPublicCoverage',
-                        'ACSTravelTime', 'ACSIncome',
-                        ]:
+    for dataset_str in set(utils_experiment.dataset_names) - set('adult'):
         X, y, A, acs_data = load_transform_ACS(dataset_str, return_acs_data=True)
         # acs_data.iloc[:1000].to_csv(f'results/fairlearn-2/ACSDataset_descriptions/ACSPublicCoverage_h1000.csv')
         # acs_data.iloc[:1000].loc[:, ['HISP', 'RAC1P', 'PUBCOV']].to_csv(f'results/fairlearn-2/ACSDataset_descriptions/ACSPublicCoverage_h1000_race.csv')
@@ -24,15 +21,20 @@ if __name__ == '__main__':
         t_dict.update(dataset_name=dataset_str, size=X.shape[0], columns=X.shape[1], sensitive_attr=A.name,
                       sensitive_attr_nunique=A.nunique(), target_col=y.name, sensitive_attr_unique_values=A.unique(),
                       mem_usage=mem_usage)
-        function_list = ['mean', 'std', 'size', ('perc', lambda x : x.size / y.shape[0])]
+        function_list = ['mean', 'std', 'size', ('perc', lambda x: x.size / y.shape[0])]
         target_stats = y.groupby(A).agg(function_list)
         target_stats.loc['mean'] = target_stats.mean()
         target_stats.loc['std'] = target_stats.std()
-        target_stats.loc['all'] = target_stats.loc['all'] = y.agg(['mean', 'std', 'size', (lambda x : x.size / y.shape[0])]).rename({'<lambda>': 'perc'})
+        target_stats.loc['all'] = target_stats.loc['all'] = y.agg(
+            ['mean', 'std', 'size', (lambda x: x.size / y.shape[0])]).rename({'<lambda>': 'perc'})
         target_stats.to_csv(os.path.join(descriptions_dir, dataset_str + '_target_stats.csv'))
         desc = X.describe().join([y.describe(), A.describe()])
         desc.to_csv(os.path.join(descriptions_dir, dataset_str + 'describe.csv'))
+
+        if dataset_str in ['adult', 'ACSPublicCoverage',
+                           'ACSEmployment', ]:
+            er.dataset_str = dataset_str
+            best_params_dict = er.load_best_params(base_model_code='lr', fraction=1.)
+            t_dict.update(**best_params_dict)
         dict_list.append(t_dict.copy())
-        er.dataset_str = dataset_str
-        best_params = er.load_best_params(base_model_code='lr', fraction=1)
     pd.DataFrame(dict_list).to_csv(os.path.join(descriptions_dir, 'all_df_descriptions_summary.csv'))
