85e88ecd72c5b96aecf9188d7e3da2f5c7b13af0
Andrea Baraldi <baraldian@gmail.com>
2023-06-01 11:57:26 +0200

    add: code to clean result path from wrongly generated folders

---
+++ run_experiments/clean_results.py
@@ -0,0 +1,19 @@
+import os
+
+import send2trash
+
+base_plot_dir = os.path.join('..','results', 'plots')
+
+
+for f in os.scandir(base_plot_dir):
+    if not f.is_dir():
+        continue
+    for f2 in os.scandir(f):
+        if not f2.is_dir():
+            continue
+        for f3 in os.scandir(f2):
+            if f3.is_dir():
+                send2trash.send2trash(f3)
+
+
+    pass
