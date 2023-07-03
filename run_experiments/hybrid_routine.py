38ef8a0b3485179f8a06f17294f9251a9ec5c343
Andrea Baraldi <baraldian@gmail.com>
2023-07-03 11:52:06 +0200

    add: update experiment execution file.

--- run_experiments/hybrid_routine.py
+++ run_experiments/hybrid_routine.py
@@ -5,6 +5,58 @@ import run
 import utils_experiment
 
 if __name__ == "__main__":
-    run.launch_experiment_by_id('acs_h_gs1_1.0')
-    run.launch_experiment_by_id('acs_h_gs1_EO_1.0')
+    # run.launch_experiment_by_id('s_h_1.0.TEST')
+    # run.launch_experiment_by_id('acs_h_gs1_EO_1.0')
 
+    # run.launch_experiment_by_id('acs_h_eps_1.0')
+
+    # run.launch_experiment_by_id('acs_h_eps_1.E0')
+    # run.launch_experiment_by_id('acs_h_eps_1.LGBM0')
+    # run.launch_experiment_by_id('sigmod_h_exp_1.0')
+    # run.launch_experiment_by_id('acs_h_gs1_EO_1.0')
+    # run.launch_experiment_by_id('s_h_EO_1.0')
+    # run.launch_experiment_by_id('acs_eps_EO_1.0')
+
+    # doing
+    # run.launch_experiment_by_id('acsE_h_gs1_1.0')
+
+    done_conf = [
+        'sigmod_h_exp_1.0',
+        's_h_exp_EO_1.0',
+        's_h_EO_1.0',
+        'acs_eps_EO_1.0',
+        'acs_eps_EO_1.1',
+        'acs_eps_EO_2.0',
+        'acs_eps_EO_2.1',
+        'acs_h_gs1_EO_2.0',
+        'acs_h_gsSR_1.0',
+        'acs_h_gsSR_2.0',
+        'acs_h_gsSR_1.1',
+        'acsE_h_gsSR_1.1',
+        'acsE_h_gs1_1.0',
+        'f_eta0_1.0',
+        'f_eta0_2.0',
+        's_h_EO_1.0',
+        'acs_h_gsSR_2.1',
+        'acs_h_gs1_1.1',
+        'sigmod_h_exp_2.0',
+        'sigmod_h_exp_3.0',  # to update
+        'acs_eps_EO_2.0',
+        'acs_eps_EO_2.1',
+        'acs_h_gs1_EO_1.0',
+
+        's_tr_1.1',
+        's_tr_2.0',
+        's_tr_2.1',
+    ]
+
+    conf_todo = [
+        # 'acs_h_gs1_1.test',
+        'acs_h_gs1_EO_2.0',
+        'acs_h_gsSR_2.0',
+        'acs_h_gsSR_2.1',
+
+    ]
+
+    for x in conf_todo:
+        run.launch_experiment_by_id(x)
