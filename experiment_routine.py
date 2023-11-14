f747adfea23633b0140f848eccc193a1dd12147d
Andrea Baraldi <baraldian@gmail.com>
2023-11-14 11:05:31 +0100

    add: new experiments.

--- experiment_routine.py
+++ experiment_routine.py
@@ -1,21 +1,6 @@
 import run
 
 if __name__ == "__main__":
-    # run.launch_experiment_by_id('s_h_1.0.TEST')
-    # run.launch_experiment_by_id('acs_h_gs1_EO_1.0')
-
-    # run.launch_experiment_by_id('acs_h_eps_1.0')
-
-    # run.launch_experiment_by_id('acs_h_eps_1.E0')
-    # run.launch_experiment_by_id('acs_h_eps_1.LGBM0')
-    # run.launch_experiment_by_id('sigmod_h_exp_1.0')
-    # run.launch_experiment_by_id('acs_h_gs1_EO_1.0')
-    # run.launch_experiment_by_id('s_h_EO_1.0')
-    # run.launch_experiment_by_id('acs_eps_EO_1.0')
-
-    # doing
-    # run.launch_experiment_by_id('acsE_h_gs1_1.0')
-
     done_conf = [
         # done on fairlearn-2
         'sigmod_h_exp_1.0',
@@ -59,8 +44,6 @@ if __name__ == "__main__":
         'acs_h_gs1_EO_1.0r',
         'acs_h_gsSR_1.0r',
         'acs_h_gsSR_EO_1.0r',
-
-
         's_c_1.0r',
         's_tr_1.0r',
         's_tr_1.1r',
@@ -69,24 +52,22 @@ if __name__ == "__main__":
         'f_eta0_eps.1',
         'f_eta0_eps.2',
         'most_frequent_sig.0r',
-
         'most_frequent_ACS.0r',
         'acs_to_binary1.0r',
         'acs_to_binaryEO1.0r',
-
-
         'acsE_eps_EO_1.0r',
         'acsE_h_eps_1.0r',
         'f_eta0_1.1',
         'f_eta0_2.1',
-
         'acsE_h_gs1_1.0r',
-
+        'acsE_h_gs1_EO_1.0r',
+        'acsE_h_gsSR_1.0r',
+        'acsE_h_gsSR_1.1r',
 
 
         # doing in fairlearn-2
-        'acsE_h_gs1_EO_1.0r',
-        #
+        'acsE_h_gs1_EO_1.1r',
+
 
         # done on fairlearn-3
         's_zDI_1.2',
@@ -94,26 +75,34 @@ if __name__ == "__main__":
         's_zEO_1.1',
         's_f_1.0r',
         's_f_1.1r',
+        'acsER_bin2.0r',
+        'acsER_bin2.1r',
+        'acsER_bin2.2r',
+        'acs_to_binary1.1r',
+        'acsER_bin3.0r',
+        'acsER_bin3.1r',
+        'acsE_h_gs1_1.1r',
+        'acsE_h_gs1_1.2r',
+
         # to update on fairlearn-3
 
 
         # doing on fairlearn-3
-        # 'acs_to_binary1.1r',
-
 
     ]
 
+
+
     conf_todo = [
-        # 'acs_h_gs1_1.test',
-        #'f_eta0_1.0.test',
-
-
+        # next on fairlearn-2
+        # 'acsER_bin3.0rtest',
         # next on fairlearn-3
 
-        'acsER_bin1.0r',
-        # 's_f_1.1r',
-        # testing
 
+
+        # testing
+        # 'acs_h_gs1_1.test',
+        # 'f_eta0_1.0.test',
 
 
     ]
